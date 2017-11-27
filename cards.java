88797dd839a417bfbc5769222ad6650f34b05111
anastafj197 <frankie.anastasia@gmail.com>
2017-11-26 20:11:02 -0500

    Added build deck and shuffle deck

--- cards.java
+++ cards.java
@@ -8,6 +8,25 @@ public class Cards {
 
   public static void main (String [] args) {
 
+  	String[] ranks = {"2","3","4","5","6","7","8","9",
+  					   "10","Jack","Queen","King","Ace"};
+
+  	String[] suits = {"Hearts","Diamonds","Clubs","Spades"};
+
+  	List<String> deck = new ArrayList<String>();
+
+  	// Build a sorted deck of cards 
+  	for (String rank : ranks) {
+  	  for (String suit : suits) {
+  	  	deck.add(rank + " of " + suit);
+  	  }
+  	}
+
+  	// Shuffle all the items in the list
+  	collections.shuffle(deck);
+
+  	System.out.println("Top card is the " + deck.get(0));
   
   }
+
 }
